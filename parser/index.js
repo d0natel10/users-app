fb9e0c1da8955778d59dfcc5220ccf521fa240f3
d0natel10 <lyamtsev03@mail.ru>
2024-11-22 03:16:50 +0300

    add: parser

---
+++ parser/index.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+(async () => {
+    try {
+        const response = await axios.get('http://127.0.0.1:8080/api/users/getUsers')
+        console.log(response.data);
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+    
+})()
